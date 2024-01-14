13abe1c10809d158e98b1bc5740f7c61eea3e3c5
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-14 14:45:15 +0300

    fix: change sequence cells

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -7,9 +7,11 @@ const ChessBoard: React.FC = () => {
 
     const paintCells = useMemo(() => () => {
         const arrayCellS = [];
+        let countRow = false;
         for (let i = 1; i < 65; i++) {
-            if (i % 2 == 0) arrayCellS.push(<Cell color={'dark'} />)
-            else arrayCellS.push(<Cell color={'light'} />)
+            if (i % 2 == 0) arrayCellS.push(<Cell color={countRow ? "light" : 'dark'} />)
+            else arrayCellS.push(<Cell color={countRow ? "dark" : 'light'} />)
+            if (i % 8 == 0) countRow = !countRow;
         }
         return (
             <Fragment>
